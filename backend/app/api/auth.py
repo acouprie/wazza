b65cb448155632dbd1aa0d3cd4bcaa59f84643ca
acouprie <antoine@couprie.dev>
2023-02-20 14:58:03 +0100

    refactor backend

---
+++ backend/app/api/auth.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from app.api.dependencies.userManager import auth_backend, current_active_user, fastapi_users
+from app.schemas.user import UserRead, UserCreate
+
+router = APIRouter()
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,14 @@
 from fastapi import Depends, FastAPI
 
-from app.db import User, create_db_and_tables
-from app.schema import UserCreate, UserRead, UserUpdate
-from app.users import auth_backend, current_active_user, fastapi_users
+from app.databases.postgres import User, create_db_and_tables
+from app.api.dependencies.userManager import auth_backend, current_active_user, fastapi_users
+from app.api.auth import router as auth_router
+from app.api.users import router as users_router
 
 app = FastAPI()
 
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
-)
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(users_router, prefix="/users", tags=["users"])
 
 @app.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
